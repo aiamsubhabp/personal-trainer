90ca7365bf440fc58eab5d3792470c4727e7474d
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-04-23 00:36:47 -0400

    add password hash

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
-from config import db, metadata
+from config import db, metadata, bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
 
 client_workout_programs = db.Table(
     'client_workout_programs',
@@ -7,6 +8,35 @@ client_workout_programs = db.Table(
     db.Column('client_id', db.Integer, db.ForeignKey('clients.id'), primary_key = True),
     db.Column('workout_program_id', db.Integer, db.ForeignKey('workout_programs.id'), primary_key = True)
 )
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    # serialize rules
+    serialize_rules = ('-sessions.user', '-_password_hash')
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique = True, nullable = False)
+    _password_hash = db.Column(db.String)
+
+    # set up one to many relationship wth Sessions
+    sessions = db.relationship('Session', back_populates = 'user')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+    
+    def __repr__(self):
+        return f'User: {self.username}, ID: {self.id}'
+
 
 class Client(db.Model, SerializerMixin):
     __tablename__ = 'clients'
@@ -50,5 +80,8 @@ class Session(db.Model, SerializerMixin):
 
     client = db.relationship('Client', back_populates = 'sessions')
     workout_program = db.relationship('WorkoutProgram', back_populates = 'sessions')
+
+    # User relationship
+    user = db.relationship('User', back_populates = 'sessions')
 
 
